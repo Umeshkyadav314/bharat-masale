e051d99e3df095a25c6cc8acfed1c595c42470e5
Umeshkyadav314 <uy429534@gmail.com>
2026-02-17 12:13:15 +0530

    fix errors

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "cdn.coverr.co" },
-      { protocol: "https", hostname: "picsum.photos" },
-    ],
-  },
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
